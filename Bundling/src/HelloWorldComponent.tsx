ca9218d13aab4ab6023bd97dbef77ca06f002c9c
Javier_GR <javigallego99@gmail.com>
2022-07-09 16:14:34 +0200

    added 2 styles

--- Bundling/src/HelloWorldComponent.tsx
+++ Bundling/src/HelloWorldComponent.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import mystyle2 from "./mystyles2.scss"
+import mystyle1 from "./mystyles.scss"
 
 export const HelloWorldComponent = () => {
   return (
     <>
         {/* {console.log(`Api base: ${process.env.API_BASE}`)} */}
+        <h1 className={mystyle1.redBackground}>Hello World from React DOM styled with SASS</h1>
         <h1 className={mystyle2.greyBackground}>Hello World from React DOM styled with SASS</h1>
     </>
   );
